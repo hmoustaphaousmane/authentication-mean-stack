2e3677012e9be1026f1aa0f1150afb9a53bc0f6e
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2025-09-06 17:49:06 +0100

    build: create user and otp models

---
+++ server/schema/user.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const userModel = mongoose.model("users", schema);
+
+module.exports = userModel;
